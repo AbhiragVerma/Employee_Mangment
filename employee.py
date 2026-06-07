3a78200c90b97982ea3742eca1b29cbd7a62b281
Abhirag <abhiragverma1@gmail.com>
2026-06-07 17:00:24 +0530

    new class

--- employee.py
+++ employee.py
@@ -71,3 +71,53 @@ class FullTimeEmployee(Employee):
             "monthly_salary": self._monthly_salary
         }
     
+class PartTimeEmployee(Employee):
+
+    def __init__(
+        self,
+        employee_id,
+        name,
+        department,
+        hourly_rate,
+        hours_worked_per_month
+    ):
+        super().__init__(employee_id, name, department)
+
+        self._hourly_rate = hourly_rate
+        self._hours_worked_per_month = hours_worked_per_month
+
+    @property
+    def hourly_rate(self):
+        return self._hourly_rate
+
+    @hourly_rate.setter
+    def hourly_rate(self, hourly_rate):
+        self._hourly_rate = hourly_rate
+
+    @property
+    def hours_worked_per_month(self):
+        return self._hours_worked_per_month
+
+    @hours_worked_per_month.setter
+    def hours_worked_per_month(self, hours_worked_per_month):
+        self._hours_worked_per_month = hours_worked_per_month
+
+    def calculate_salary(self):
+        return self._hourly_rate * self._hours_worked_per_month
+
+    def display_details(self):
+        return (
+            f"{super().display_details()}, "
+            f"Hourly Rate: {self._hourly_rate}, "
+            f"Hours Worked: {self._hours_worked_per_month}"
+        )
+
+    def to_dict(self):
+        return {
+            "type": "parttime",
+            "employee_id": self._employee_id,
+            "name": self._name,
+            "department": self._department,
+            "hourly_rate": self._hourly_rate,
+            "hours_worked_per_month": self._hours_worked_per_month
+        }
